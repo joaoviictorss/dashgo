1de29048add1602738fb75693ebc56eed2c5b3f1
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-04-08 10:42:36 -0300

    feat: SignIn page created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { Input } from "../components/Form/Input";
 
-const inter = Inter({ subsets: ["latin"] });
-
-export default function Home() {
+export default function SignIn() {
   return (
-    <h1>
-     Hello World!
-    </h1>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        w="100%"
+        maxW={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <Input name="email" label="E-mail" type="email" />
+          <Input name="password" label="Senha" type="password" />
+        </Stack>
+
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
